b62b89bcc881ee7364899ea7772ca172fc7aafd0
OurAppWork <shaizi.assasin@hotmail.com>
2016-07-29 00:53:45 +0500

    Start

---
+++ app/src/main/java/com/sappx/namazalert/MainActivity.java
@@ -0,0 +1,28 @@
+package com.sappx.namazalert;
+
+import android.preference.CheckBoxPreference;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+
+//    CheckBox fajr = null;
+//    CheckBox zohr = null;
+//    CheckBox asar = null;
+//    CheckBox magrib = null;
+//    CheckBox isha = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        fajr = (CheckBox)findViewById(R.id.checkfajr);
+//        zohr = (CheckBox)findViewById(R.id.checkzohr);
+//        asar = (CheckBox)findViewById(R.id.checkasar);
+//        magrib = (CheckBox)findViewById(R.id.checkmagrib);
+//        isha = (CheckBox)findViewById(R.id.checkisha);
+
+    }
+}
